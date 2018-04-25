e3df6eca028b85d101e8687c27e1c65ead8b0c07
Keith Mifsud <keithmifsud@users.noreply.github.com>
2018-04-25 11:47:27 +0100

    Gh 60 Social Sharing Bugs and Code Refactoring (#67)
    * V2 of existing blog post.
    
    * Updated the content as per PR #GH-45
    
    * Several fixes as per PR review
    
    * Added a reverse loop to blog list
    
    * diagrams and illustrations
    
    * included resources and guideline
    
    * tweaked title
    
    * updated opacity
    
    * GH-60 Developed a twitter share script. Not refactored YET!
    
    * GH-60 Refactored the JS for opening the share window.
    
    * GH-60 Reafctored the Twitter JS
    
    * GH-60 Changed the share links to use the page.url

--- _includes/social-share/socialSharing.js
+++ _includes/social-share/socialSharing.js
@@ -4,41 +4,19 @@
  * Forces the share links to open in a new and sized
  * window.
  */
+function openShareWindow (element, width, height) {
 
-function shareOnFacebook () {
-	window.open(
-		'http://www.facebook.com/sharer.php?u={{ site.url }}{{ page.url }}',
-		'shareWindow',
-		'location=1,toolbar=1,menubar=1,resizable=1,width=200,height=200'
-	)
-	return false
-}
+	var href = element.getAttribute('href')
+	if (element.getAttribute('id') === 'twitter-share') {
+		href = getTwitterUrl()
+	}
 
-function shareOnTwitter () {
 	window.open(
-		'https://twitter.com/share?url={{ site.url }}{{ page.url }}',
+		href,
 		'shareWindow',
-		'location=1,toolbar=1,menubar=1,resizable=1,width=400,height=300'
+		'location=1,toolbar=1,menubar=1,resizable=1,width=' + (width || 400) +
+		',height=' + (height || 300)
 	)
-	return false
-}
-
-function shareOnGooglePlus () {
-	window.open(
-		'https://plus.google.com/share?url={{ site.url }}{{ page.url }}',
-		'shareWindow',
-		'location=1,toolbar=1,menubar=1,resizable=1,width=400,height=300'
-	)
-	return false
-}
-
-function shareOnLinkedIn () {
-	window.open(
-		'http://www.linkedin.com/shareArticle?mini=true&amp;url={{ site.url }}{{ page.url }}',
-		'shareWindow',
-		'location=1,toolbar=1,menubar=1,resizable=1,width=400,height=300'
-	)
-	return false
 }
 
 /**
@@ -114,8 +92,114 @@ function isSmallScreen () {
 }
 
 /**
+ * Gets the url for sharing on Twitter.
+ *
+ * @return {string}
+ */
+function getTwitterUrl () {
+	return 'https://twitter.com/intent/tweet?text=' + getTweetText() +
+		'&via=' + getTwitterSetting('username') + '&url=' + getArticleUrl()
+}
+
+/**
+ * Gets the formatted twitter URL query text parameter
+ *
+ * @return string
+ */
+function getTweetText () {
+	var title = getArticleTitle()
+
+	if (tweetExceedsAllowedLength()) {
+		title = trimArticleTitleForTweet()
+	}
+
+	title = title.split(' ').join('+')
+	return title
+}
+
+/**
+ * Trims the article length to fit in a tweet.
+ *
+ * @return {string}
+ */
+function trimArticleTitleForTweet () {
+	var title = getArticleTitle()
+	var moreText = ' ..'
+	var numberOfCharsToTrim = calculateNumberOfCharactersToTrimFromArticleTitle()
+		+ moreText.length
+
+	title = title.substr(0, (title.length - numberOfCharsToTrim))
+	return title + moreText
+}
+
+/**
+ * Checks if the tweet's content exceeds the
+ * standard tweet amount, taking into account
+ * the username and the url.
+ *
+ * @return {boolean}
+ */
+function tweetExceedsAllowedLength () {
+	var allowedTweetLength = getTwitterSetting('allowed_tweet_length')
+	var articleTitleLength = getArticleTitle().length + 1 // trailing space
+	var shortenedUrlLength = getTwitterSetting('shortened_url_length') + 1 // trailing space
+	var userNameLength = getTwitterSetting('username_display_length')
+	var tweetLength = articleTitleLength + shortenedUrlLength + userNameLength
+	return tweetLength > allowedTweetLength
+}
+
+/**
+ * Gets the number of available characters within a tweet
+ * after reducing the url and username.
+ *
+ * @return {number}
+ */
+function calculateNumberOfCharactersToTrimFromArticleTitle () {
+	var allowedTweetLength = getTwitterSetting('allowed_tweet_length')
+	var articleTitleLength = getArticleTitle().length + 1 // trailing space
+	var shortenedUrlLength = getTwitterSetting('shortened_url_length') + 1 // trailing space
+	var userNameLength = getTwitterSetting('username_display_length')
+	var availableCharacters = allowedTweetLength - shortenedUrlLength -
+		userNameLength
+
+	return articleTitleLength - availableCharacters
+}
+
+/**
+ * Gets the twitter setting.
+ *
+ * @param name
+ * @return {*}
+ */
+function getTwitterSetting (name) {
+	var settings = {
+		'allowed_tweet_length': 280,
+		'shortened_url_length': 23,
+		'username_display_length': 13, // via @learnk8s
+		'username': 'learnk8s'
+	}
+	return settings[name]
+}
+
+/**
+ * Gets the article's url.
+ * @return {string}
+ */
+function getArticleUrl () {
+	return '{{ page.url | absolute_url }}'
+}
+
+/**
+ * Gets the Article's title.
+ *
+ * @return {string}
+ */
+function getArticleTitle () {
+	return '{{page.title}}'
+}
+
+/**
  * Calls the actOnScroll method when
  * the window is scrolling.
  */
 window.onscroll = function () {actOnScroll()}
-
