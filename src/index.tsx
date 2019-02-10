220c0e9dfaac1fd3a8363551488efbc032a55a04
Daniele Polencic <daniele.polencic@gmail.com>
2019-02-10 17:54:10 +0000

    using typescript to generate pages

---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+import { LinkedNode, Page, PageType, getFullUrl, sitemap, TrainingPage } from './sitemap'
+import * as Sitemap from './sitemap'
+import { Homepage, assets as assetsHomepage } from './homepage'
+import { Training, assets as assetsTraining } from './training'
+import React from 'react'
+import {renderToStaticMarkup} from 'react-dom/server'
+import { writeFileSync } from 'fs'
+import { resolve } from 'path'
+import { optimiseAssets } from './assets';
+import { mkdir } from 'shelljs'
+
+export function run(options: {siteUrl: string}) {
+  return function mount(root: LinkedNode<Page>) {
+    renderTree(root, root)
+  }
+
+  function renderTree(node: LinkedNode<Page>, root: LinkedNode<Page>) {
+    render(node, root, options)
+    node.children.forEach(it => renderTree(it as any, root))
+  }
+}
+
+function render(node: LinkedNode<Page>, root: LinkedNode<Page>, {siteUrl}: {siteUrl: string}) {
+  const page = node.payload
+  const path = `_site${resolve('.', getFullUrl(node), 'index.html')}`
+  mkdir('-p', `_site${resolve('.', getFullUrl(node))}`)
+  switch (page.type) {
+    case PageType.HOMEPAGE: {
+      writeFileSync(path, renderToStaticMarkup(<Homepage root={root} currentPage={node as LinkedNode<Sitemap.Homepage>} siteUrl={siteUrl} assets={optimiseAssets(assetsHomepage)} />))
+      return
+    }
+    case PageType.TRAINING: {
+      writeFileSync(path, renderToStaticMarkup(<Training root={root} currentPage={node as LinkedNode<TrainingPage>} siteUrl={siteUrl} assets={optimiseAssets(assetsTraining)} />))
+      return
+    }
+    default:
+      // assertUnreachable(page)
+  }
+}
+
+function assertUnreachable(x: never): never {
+  throw new Error('Did not expect to get here')
+}
+
+run({siteUrl: 'https://learnk8s.io'})(sitemap)
