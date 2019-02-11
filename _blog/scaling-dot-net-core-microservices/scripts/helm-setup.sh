478305b7078ece2a122549f8f732bd757b201aa9
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-02-11 14:45:45 +0000

    Helm setup script

---
+++ _blog/scaling-dot-net-core-microservices/scripts/helm-setup.sh
@@ -0,0 +1,4 @@
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+kubectl get pods --namespace kube-system
