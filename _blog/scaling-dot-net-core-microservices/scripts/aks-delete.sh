06f7fd60ebd700685d42c99da6e8d4182746669b
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-02-11 14:36:36 +0000

    Update delete scripts to clear local kubectl

--- _blog/scaling-dot-net-core-microservices/scripts/aks-delete.sh
+++ _blog/scaling-dot-net-core-microservices/scripts/aks-delete.sh
@@ -3,4 +3,7 @@
 source variables.sh
 
 az group delete --name ${RESOURCE_GROUP} --yes --no-wait
+kubectl config unset users.clusterUser_${RESOURCE_GROUP}_${CLUSTER_NAME}
 kubectl config unset contexts.${CLUSTER_NAME}
+kubectl config unset clusters.${CLUSTER_NAME}
+
--- _blog/scaling-dot-net-core-microservices/scripts/variables.sh
+++ _blog/scaling-dot-net-core-microservices/scripts/variables.sh
@@ -1,4 +1,3 @@
 export RESOURCE_GROUP=ticketingGroup
 export CLUSTER_NAME=ticketingAKSCluster
 export AKS_VM_GROUP=MC_${RESOURCE_GROUP}_${CLUSTER_NAME}_EASTUS
-
