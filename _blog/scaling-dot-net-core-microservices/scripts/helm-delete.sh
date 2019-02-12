735457ba684287031db1b5a8eac1616c7f0dab04
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-02-12 09:31:51 +0000

    Update helm scripts

---
+++ _blog/scaling-dot-net-core-microservices/scripts/helm-delete.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+helm reset --force
--- _blog/scaling-dot-net-core-microservices/scripts/helm-setup.sh
+++ _blog/scaling-dot-net-core-microservices/scripts/helm-setup.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-kubectl -n kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+kubectl apply -f helm-rbac.yaml
 helm init --service-account tiller
 kubectl get pods --namespace kube-system
