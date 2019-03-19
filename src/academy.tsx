17613e191a34caaf01318d6106b03fec7877779f
Daniele Polencic <daniele.polencic@gmail.com>
2019-03-19 20:17:55 +0800

    add inspectlet to the academy

--- src/academy.tsx
+++ src/academy.tsx
@@ -113,6 +113,18 @@ export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>
         paths: ['node_modules/tachyons/css/tachyons.css', 'assets/style.css'],
       })}
     >
+      <script
+        type='text/javascript'
+        dangerouslySetInnerHTML={{
+          __html: `(function() {
+window.__insp = window.__insp || [];
+__insp.push(['wid', 1190458479]);
+var ldinsp = function(){
+if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=1190458479&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+setTimeout(ldinsp, 0);
+})();`,
+        }}
+      />
       <JsonLd<Course>
         item={{
           '@type': 'Course',
