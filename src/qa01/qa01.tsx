a1d639f1c260c66388deceb2f55048f45975958a
Daniele Polencic <daniele.polencic@gmail.com>
2019-03-23 00:01:14 +0800

    ask the expert: march

---
+++ src/qa01/qa01.tsx
@@ -0,0 +1,94 @@
+import { Image, CSSBundle, JSScript, JSBundle } from '../assets'
+import { Sitemap, LinkedNode, getAbsoluteUrl, getFullUrl } from '../sitemap'
+import * as React from 'react'
+import { Markdown, Article, RelatedConentContainer, RelatedContentItem } from '../article'
+import { cat } from 'shelljs'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { JsonLd } from 'react-schemaorg'
+import { BlogPosting } from 'schema-dts'
+import { ListItem, PromoAcademy } from '../layout'
+
+export const Details = {
+  type: identity<'qa01'>('qa01'),
+  url: '/top-questions-march-2019',
+  seoTitle: 'Kubernetes Top Questions in March 2019 ♦︎ Learnk8s',
+  title: 'Kubernetes Top Questions in March 2019',
+  shortDescription: ``,
+  description: ``,
+  openGraphImage: Image({
+    url: 'src/k8sOnWindows/k8s_on_win.jpg',
+    description: 'placeholder',
+  }),
+  publishedDate: '2019-01-09',
+  previewImage: Image({
+    url: 'src/k8sOnWindows/k8s_on_win.jpg',
+    description: 'placeholder',
+  }),
+  author: {
+    fullName: 'Daniele Polencic',
+    avatar: Image({ url: 'assets/authors/daniele_polencic.jpg', description: 'Daniele Polencic' }),
+    link: 'https://twitter.com/danielepolencic',
+  },
+}
+
+function identity<T>(value: T): T {
+  return value
+}
+
+export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>, siteUrl: string): string {
+  const { css, js, html } = Markdown(cat('src/qa01/content.md').toString(), __dirname)
+  return renderToStaticMarkup(
+    <Article
+      website={website}
+      seoTitle={currentNode.payload.seoTitle}
+      title={currentNode.payload.title}
+      description={currentNode.payload.shortDescription}
+      openGraphImage={currentNode.payload.openGraphImage}
+      absolutUrl={getAbsoluteUrl(currentNode, siteUrl)}
+      authorFullName={currentNode.payload.author.fullName}
+      authorAvatar={currentNode.payload.author.avatar}
+      authorLink={currentNode.payload.author.link}
+      cssBundle={CSSBundle({
+        paths: [
+          'node_modules/tachyons/css/tachyons.css',
+          'src/prism-solarizedlight.css',
+          'src/prism-line-highlight.css',
+          'assets/style.css',
+        ],
+        styles: css,
+      })}
+      publishedDate={currentNode.payload.publishedDate}
+    >
+      <JsonLd<BlogPosting>
+        item={{
+          '@context': 'https://schema.org',
+          '@type': 'BlogPosting',
+          headline: currentNode.payload.title,
+          image: `${siteUrl}${currentNode.payload.previewImage.url}`,
+          author: {
+            '@type': 'Person',
+            name: currentNode.payload.author.fullName,
+          },
+          publisher: {
+            '@type': 'Organization',
+            name: 'Learnk8s',
+            logo: {
+              '@type': 'ImageObject',
+              url: `${siteUrl}${Image({ url: 'assets/learnk8s_logo_square.png', description: 'Learnk8s logo' }).url}`,
+            },
+          },
+          url: getAbsoluteUrl(currentNode, siteUrl),
+          datePublished: currentNode.payload.publishedDate,
+          dateModified: currentNode.payload.publishedDate,
+          description: currentNode.payload.description,
+          mainEntityOfPage: {
+            '@type': 'SoftwareSourceCode',
+          },
+        }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+
+      <PromoAcademy sitemap={website} />
+    </Article>,
+  )
+}
