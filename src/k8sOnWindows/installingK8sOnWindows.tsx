4b89f102297cce79366319147ccf5f4e9cda9564
Daniele Polencic <daniele.polencic@gmail.com>
2019-04-23 17:01:14 +0800

    migrated k8s on win

--- src/k8sOnWindows/installingK8sOnWindows.tsx
+++ src/k8sOnWindows/installingK8sOnWindows.tsx
@@ -1,43 +1,31 @@
-import { Image, CSSBundle } from '../assets'
 import { Sitemap, LinkedNode, getAbsoluteUrl } from '../sitemap'
 import * as React from 'react'
-import { Article } from '../article'
+import { Article } from '../article.v2'
 import { renderToStaticMarkup } from 'react-dom/server'
 import { JsonLd } from 'react-schemaorg'
 import { BlogPosting } from 'schema-dts'
-import { Subscribe } from '../layout'
-import * as Remark from '../remark'
+import { Subscribe } from '../layout.v2'
+import * as Remark from '../remark.v2'
 
 export const Details = {
-  type: identity<'k8sOnWindows'>('k8sOnWindows'),
+  type: 'k8sOnWindows',
   url: '/installing-docker-and-kubernetes-on-windows',
   seoTitle: 'Getting started with Docker and Kubernetes on Win 10 ♦︎ Learnk8s',
   title: 'Getting started with Docker and Kubernetes on Windows 10',
   shortDescription: `In this article you'll learn how to make the right choices when it comes to setting up your development environment on Windows.`,
   description: `Getting started with Docker and Kubernetes on Windows can be daunting when you don't know where to begin. In this article you'll learn how to make the right choices when it comes to setting up your development environment on Windows.`,
-  openGraphImage: Image({
-    url: 'src/k8sOnWindows/k8s_on_win.jpg',
-    description: 'Getting started with Docker and Kubernetes on Windows 10',
-  }),
+  openGraphImage: <img src='src/k8sOnWindows/k8s_on_win.jpg' alt='Getting started with Docker and Kubernetes on Windows 10' />,
   publishedDate: '2018-06-05',
   lastModifiedDate: '2019-03-20',
-  previewImage: Image({
-    url: 'src/k8sOnWindows/k8s_on_win.jpg',
-    description: 'Getting started with Docker and Kubernetes on Windows 10',
-  }),
+  previewImage: <img src='src/k8sOnWindows/k8s_on_win.jpg' alt='Getting started with Docker and Kubernetes on Windows 10' />,
   author: {
     fullName: 'Keith Mifsud',
-    avatar: Image({ url: 'assets/authors/keith_mifsud.jpg', description: 'Keith Mifsud' }),
+    avatar: <img src='assets/authors/keith_mifsud.jpg' alt='Keith Mifsud' />,
     link: 'https://keith-mifsud.me/',
   },
-}
-
-function identity<T>(value: T): T {
-  return value
-}
+} as const
 
 export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>, siteUrl: string): string {
-  const { css, js, html } = Remark.render(`${__dirname}/content.md`)
   return renderToStaticMarkup(
     <Article
       website={website}
@@ -49,10 +37,6 @@ export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>
       authorFullName={currentNode.payload.author.fullName}
       authorAvatar={currentNode.payload.author.avatar}
       authorLink={currentNode.payload.author.link}
-      cssBundle={CSSBundle({
-        paths: ['node_modules/tachyons/css/tachyons.css', 'assets/style.css'],
-        styles: css,
-      })}
       publishedDate={currentNode.payload.publishedDate}
       lastUpdated={currentNode.payload.lastModifiedDate}
     >
@@ -61,7 +45,7 @@ export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>
           '@context': 'https://schema.org',
           '@type': 'BlogPosting',
           headline: currentNode.payload.title,
-          image: `${siteUrl}${currentNode.payload.previewImage.url}`,
+          image: `${currentNode.payload.previewImage.props.src}`,
           author: {
             '@type': 'Person',
             name: currentNode.payload.author.fullName,
@@ -71,7 +55,7 @@ export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>
             name: 'Learnk8s',
             logo: {
               '@type': 'ImageObject',
-              url: `${siteUrl}${Image({ url: 'assets/learnk8s_logo_square.png', description: 'Learnk8s logo' }).url}`,
+              url: `assets/learnk8s_logo_square.png`,
             },
           },
           url: getAbsoluteUrl(currentNode, siteUrl),
@@ -83,7 +67,7 @@ export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>
           },
         }}
       />
-      {html}
+      {Remark.render(`${__dirname}/content.md`)}
 
       <Subscribe identifier='install-win-docker-k8s' />
     </Article>,
