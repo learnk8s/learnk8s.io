c31918f4609db3446c194f7f65f94c8aa33e8854
Daniele Polencic <daniele.polencic@gmail.com>
2019-04-23 16:53:47 +0800

    migrated laravel article

--- src/index.tsx
+++ src/index.tsx
@@ -200,7 +200,14 @@ function render(node: LinkedNode<any>, root: Sitemap, { siteUrl }: Settings) {
       return
     }
     case DeployLaravel.Details.type: {
-      writeFileSync(generatePath(), `<!DOCTYPE html>${DeployLaravel.render(root, node, siteUrl)}`)
+      const $ = Cheerio.of(DeployLaravel.render(root, node, siteUrl))
+      isOptimisedBuild ? optimiseImages({ $, siteUrl }) : rewriteImages({ $ })
+      isOptimisedBuild ? injectGoogleAnalytics({ $, gaId: 'GTM-5WCKPRL' }) : null
+      optimiseCss({ $ })
+      optimiseJs({ $ })
+      isOptimisedBuild ? optimiseFavicons({ $ }) : rewriteFavicons({ $ })
+      isOptimisedBuild ? optimiseOpenGraphImage({ $, siteUrl }) : rewriteOpenGraphImage({ $ })
+      writeFileSync(generatePath(), $.html())
       return
     }
     case ChaosEngineering.Details.type: {
