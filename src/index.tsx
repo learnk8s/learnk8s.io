147b80c60ca5a469560b28be194bafb3e20211ee
Daniele Polencic <daniele.polencic@gmail.com>
2019-04-23 17:33:48 +0800

    migrated solar plants

--- src/index.tsx
+++ src/index.tsx
@@ -240,7 +240,14 @@ function render(node: LinkedNode<any>, root: Sitemap, { siteUrl }: Settings) {
       return
     }
     case SolarPlants.Details.type: {
-      writeFileSync(generatePath(), `<!DOCTYPE html>${SolarPlants.render(root, node, siteUrl)}`)
+      const $ = Cheerio.of(SolarPlants.render(root, node, siteUrl))
+      isOptimisedBuild ? optimiseImages({ $, siteUrl }) : rewriteImages({ $ })
+      isOptimisedBuild ? injectGoogleAnalytics({ $, gaId: 'GTM-5WCKPRL' }) : null
+      optimiseCss({ $ })
+      optimiseJs({ $ })
+      isOptimisedBuild ? optimiseFavicons({ $ }) : rewriteFavicons({ $ })
+      isOptimisedBuild ? optimiseOpenGraphImage({ $, siteUrl }) : rewriteOpenGraphImage({ $ })
+      writeFileSync(generatePath(), $.html())
       return
     }
     case WhatIsKubernetes.Details.type: {
