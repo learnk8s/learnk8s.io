902f3788a039e1eec1d0fcb2a7c303bb0721fad4
Daniel Weibel <danielmweibel@gmail.com>
2019-09-03 21:34:24 +0400

    Fix title

--- src/bsk201909/index.tsx
+++ src/bsk201909/index.tsx
@@ -4,9 +4,9 @@ import * as React from 'react'
 export const SmallOrLargeDetails = identity<Details>({
   type: 'bsk-sept-01' as const,
   url: '/kubernetes-node-size',
-  seoTitle: 'Architecting a Kubernetes clusters — choosing a node size',
-  title: 'Architecting a Kubernetes clusters — choosing a node size',
-  description: `When you create a Kubernetes cluster, one of the first questions that pops up is: "what type of worker nodes should I use and how many of them?". This article looks at the pros and cons of either using many small nodes or few large nodes.`,
+  seoTitle: 'Architecting Kubernetes clusters — choosing a worker node size',
+  title: 'Architecting Kubernetes clusters — choosing a worker node size',
+  description: `When you create a Kubernetes cluster, one of the first questions that pops up is: "what type of worker nodes should I use, and how many of them?". This article looks at the pros and cons of either using many small or few large worker nodes in your cluster.`,
   openGraphImage: <img src='assets/bsk.png' alt='Bite-sized Kubernetes learning' />,
   publishedDate: '2019-09-04',
   previewImage: <img src='assets/bsk.png' alt='Bite-sized Kubernetes learning' />,
