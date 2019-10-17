ac0d1b394df7b243f8139b468d84d5537c7ab9ce
Daniele Polencic <daniele.polencic@gmail.com>
2019-10-17 09:55:49 +0800

    added facebook app id

--- src/layout.v2.tsx
+++ src/layout.v2.tsx
@@ -28,6 +28,7 @@ export const Layout: React.StatelessComponent<{
         <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:site' content='@learnk8s' />
+        <meta property='fb:app_id' content='398212777530104' />
         <link rel='apple-touch-icon' sizes='180x180' href='assets/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='assets/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='assets/favicon-16x16.png' />
