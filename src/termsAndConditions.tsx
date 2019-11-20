2b49f070312796351a31f40725eabfec8887124a
Patrick Low <lowyuenan@gmail.com>
2019-11-20 09:24:42 +0800

    migrate consulting page

--- src/termsAndConditions.tsx
+++ src/termsAndConditions.tsx
@@ -96,34 +96,3 @@ function renderPage(state: State) {
     </Html>
   )
 }
-
-// export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>, siteUrl: string): string {
-//   return renderToStaticMarkup(
-//     <Layout
-//       website={website}
-//       seoTitle={currentNode.payload.seoTitle}
-//       title={currentNode.payload.title}
-//       description={currentNode.payload.description}
-//       openGraphImage={currentNode.payload.openGraphImage}
-//       absoluteUrl={getAbsoluteUrl(currentNode, siteUrl)}
-//     >
-//       <div className='trapezoid-1 white pt3 pt0-ns pb2 pb4-ns'>
-//         <Navbar />
-
-//         <section className='ph5-l'>
-//           <div className='w-100'>
-//             <h1 className='f1 pl3 pl4-ns f-subheadline-l'>General terms and conditions</h1>
-//             <h2 className='f4 normal measure-narrow lh-copy ph3 ph4-ns f3-l pb4'>Effective from 10 May 2018</h2>
-//           </div>
-//         </section>
-//       </div>
-
-//       <section className='bg-black-02 black-70 relative z-999 w-90-m w-70-l center pa3 pa4-ns mb3 mb5-ns lh-copy'>
-//         <div dangerouslySetInnerHTML={{ __html: marked(cat(`${__dirname}/t-and-cs.md`).toString(), { renderer }) }} />
-//       </section>
-
-//       <Consultation />
-//       <Footer root={website} />
-//     </Layout>,
-//   )
-// }
