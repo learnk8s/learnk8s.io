3c8c33629e6ba265eed0dfbf4cd4011a7a736e4d
Patrick Low <lowyuenan@gmail.com>
2019-12-02 16:52:59 +0800

    remove render from biteSized.tsx

--- src/biteSized.tsx
+++ src/biteSized.tsx
@@ -150,50 +150,6 @@ function renderPage(state: State) {
   )
 }
 
-export function render(website: Sitemap, currentNode: LinkedNode<typeof Details>, siteUrl: string): string {
-  return renderToStaticMarkup(
-    <Layout
-      website={website}
-      seoTitle={currentNode.payload.seoTitle}
-      title={currentNode.payload.title}
-      description={currentNode.payload.description}
-      openGraphImage={currentNode.payload.openGraphImage}
-      absoluteUrl={getAbsoluteUrl(currentNode, siteUrl)}
-    >
-      <div className='trapezoid-1 white pt3 pt0-ns pb2 pb4-ns'>
-        <Navbar />
-
-        <section className='ph5-l'>
-          <div className='w-100'>
-            <h1 className='f1 pl3 pl4-ns f-subheadline-l'>Latest posts</h1>
-          </div>
-        </section>
-      </div>
-
-      <section className='ph3 measure-wide pv4 center'>
-        <ul className='list pl0'>
-          {getBiteSizedSeries(website).map(it => {
-            return (
-              <li className='pv3'>
-                <h2 className='mb0'>
-                  <a href={getFullUrl(it)} className='link navy'>
-                    {it.payload.title}
-                  </a>
-                </h2>
-                <p className='black-40 mt1'>{moment(it.payload.publishedDate).format('MMMM Do YYYY')}</p>
-                <p className='lh-copy black-70'>{it.payload.description}</p>
-              </li>
-            )
-          })}
-        </ul>
-      </section>
-
-      <Consultation />
-      <Footer />
-    </Layout>,
-  )
-}
-
 function comparePublishedDate(
   postA: BlogPost & { content?: VReference<string> | undefined },
   postB: BlogPost & { content?: VReference<string> | undefined },
