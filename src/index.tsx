bfe75cbef1a56f65277acba72d8c6c62a6d0d792
Patrick Low <lowyuenan@gmail.com>
2019-12-18 09:25:05 +0800

    relocate register function

--- src/index.tsx
+++ src/index.tsx
@@ -21,65 +21,18 @@ import * as Redirect from './redirect'
 import * as RSS from './rss'
 import * as TermsAndConditions from './termsAndConditions'
 
-import * as SmallerImages from './smallerDockerImages/smallerImages'
-import * as DeployLaravel from './deployLaravel/deployLaravel'
-import * as K8sOnWindows from './k8sOnWindows/installingK8sOnWindows'
-import * as ChaosEngineering from './chaosEngineering/chaosEngineering'
-import * as SolarPlants from './solarPlants/solarPlants'
-import * as SpotInstances from './spotInstances/spotInstances'
-import * as ScalingTensorflow from './scalingKubeflow/scalingTensorflow'
-import * as ScalingSpringBoot from './scalingSpringBoot/scalingSpringBoot'
-import * as WhatIsKubernetes from './whatIsKubernetes/whatIsK8s'
-import * as AdvancedKubectl from './advancedKubectl/advancedKubectl'
-import * as TerraformAks from './terraformAks/terraformAks'
-import * as ZeroToK8sJs from './02k8sjs/02k8sjs'
-import * as Troubleshooting from './troubleshooting/troubleshooting'
 import * as GenericBlogPost from './genericBlogPost'
 
 import * as BiteSizedListing from './biteSized'
 import * as BiteSizedRenderer from './biteSized.v2'
-import * as BiteSizedArticles from './bite-sized-articles'
 import { store, getConfig } from './store'
-import * as Courses from './courses'
 import * as Training from './training.v2'
 import * as BestPractices from './best-practices/best'
 import * as Flipboard from './flipboard'
 import { checkPageDetail } from './checkPageDetail'
+import { register } from './Register'
 
-Courses.Register(store)
-Training.Register(store)
-Landing.Register(store)
-BestPractices.Register(store)
-Careers.Register(store)
-TermsAndConditions.Register(store)
-Consulting.Register(store)
-Homepage.Register(store)
-AboutUs.Register(store)
-Newsletter.Register(store)
-AdvancedKubectl.Register(store)
-Academy.Register(store)
-ContactUs.Register(store)
-ChaosEngineering.Register(store)
-Flipboard.Register(store)
-ZeroToK8sJs.Register(store)
-DeployLaravel.Register(store)
-K8sOnWindows.Register(store)
-ScalingTensorflow.Register(store)
-ScalingSpringBoot.Register(store)
-SmallerImages.Register(store)
-SolarPlants.Register(store)
-SpotInstances.Register(store)
-TerraformAks.Register(store)
-WhatIsKubernetes.Register(store)
-BiteSizedArticles.Register(store)
-RSS.Register(store)
-NotFound.Register(store)
-Sitemap.Register(store)
-Blog.Register(store)
-ArchitectingAndScaling.Register(store)
-ZeroToK8s.Register(store)
-BiteSizedListing.Register(store)
-Troubleshooting.Register(store)
+register(store)
 
 Landing.Mount({ store })
 Training.Mount({ store })
--- test.ts
+++ test.ts
@@ -1,38 +1,8 @@
 const backstop = require('backstopjs')
 import { ok } from 'assert'
-import { store, getRedirects, getPages } from './src/store'
-import { Training } from './src/training.v2'
-import { BestPractices } from './src/best-practices/best'
-import { TermsAndConditions } from './src/termsAndConditions'
-import { Consulting } from './src/consulting'
-import { AboutUs } from './src/aboutUs'
-import { Newsletter } from './src/newsletter'
-import { Academy } from './src/academy'
-import { ContactUs } from './src/contactUs'
-import { ChaosEngineering } from './src/chaosEngineering/chaosEngineering'
-import { ZeroToK8sJs } from './src/02k8sjs/02k8sjs'
-import { ZeroToK8s } from './src/zeroToK8s'
-import { Blog } from './src/blog'
-import { RSS } from './src/rss'
-import { WhatIsKubernetes } from './src/whatIsKubernetes/whatIsK8s'
-import { TerraformAks } from './src/terraformAks/terraformAks'
-import { SpotInstances } from './src/spotInstances/spotInstances'
-import { SolarPlants } from './src/solarPlants/solarPlants'
-import { SmallerImages } from './src/smallerDockerImages/smallerImages'
-import { ScalingSpringBoot } from './src/scalingSpringBoot/scalingSpringBoot'
-import { ScalingTensorflow } from './src/scalingKubeflow/scalingTensorflow'
-import { K8sOnWindows } from './src/k8sOnWindows/installingK8sOnWindows'
-import { DeployLaravel } from './src/deployLaravel/deployLaravel'
-import { Career } from './src/careers'
-import { Pages as Landing } from './src/landing'
-import { HomePage } from './src/homepage'
-import { KubectlProductivity } from './src/advancedKubectl/advancedKubectl'
-import { RSS as Flipboard } from './src/flipboard'
-import { Pages as BiteSizedPosts } from './src/bite-sized-articles'
-import { NotFound404 } from './src/404'
-import { Architecting } from './src/architecting'
-import { BiteSized } from './src/biteSized'
+import { store, getPages } from './src/store'
 import commander from 'commander'
+import { register } from './src/Register'
 
 commander
   .version('1.0.0')
@@ -45,45 +15,10 @@ ok(commander.hostname, 'Please provide a hostname such as --hostname http://loca
 ok(commander.approve || commander.test, 'Please provide either --approve or --test')
 
 function renderPage(siteUrl: string): string[] {
-  // TODO: get all pages URL
-  const urls = []
-  urls.push(Training.url)
-  Object.values(Landing).forEach(it => {
-    urls.push(it.url)
-  })
-  urls.push(BestPractices.url)
-  urls.push(Career.url)
-  urls.push(TermsAndConditions.url)
-  urls.push(Consulting.url)
-  urls.push(HomePage.url)
-  urls.push(AboutUs.url)
-  urls.push(Newsletter.url)
-  urls.push(KubectlProductivity.url)
-  urls.push(Academy.url)
-  urls.push(ContactUs.url)
-  urls.push(ChaosEngineering.url)
-  urls.push(Flipboard.url)
-  urls.push(ZeroToK8sJs.url)
-  urls.push(DeployLaravel.url)
-  urls.push(K8sOnWindows.url)
-  urls.push(ScalingTensorflow.url)
-  urls.push(ScalingSpringBoot.url)
-  urls.push(SmallerImages.url)
-  urls.push(SolarPlants.url)
-  urls.push(SpotInstances.url)
-  urls.push(TerraformAks.url)
-  urls.push(WhatIsKubernetes.url)
-  Object.values(BiteSizedPosts).forEach(it => {
-    urls.push(it.url)
-  })
-  urls.push(RSS.url)
-  urls.push(NotFound404.url)
-  urls.push(Blog.url)
-  urls.push(Architecting.url)
-  urls.push(ZeroToK8s.url)
-  urls.push(BiteSized.url)
-
-  return urls.map(it => `${siteUrl}${it}`)
+  register(store)
+  const state = store.getState()
+  const pages = getPages(state)
+  return pages.map(it => `${siteUrl}${it.url}`)
 }
 
 function toId(raw: string): string {
