edee8c2a738b04f350011c2994df0b1c4da4fa03
Daniele Polencic <daniele.polencic@gmail.com>
2020-03-10 16:36:39 +0800

    checkpoint

--- src/02k8s-java/02k8s-java.tsx
+++ src/02k8s-java/02k8s-java.tsx
@@ -7,9 +7,9 @@ import { toVFile } from '../files'
 
 export const ZeroToK8sJava = {
   id: '02k8s-java',
-  url: '/java-kubernetes-guide',
+  url: '/spring-boot-kubernetes-guide',
   title: 'Hands-on guide: developing & deploying Java apps in Kubernetes',
-  description: `Learn how to design and architect Java apps that leverage Kubernetes and scale to millions of requests.`,
+  description: `Learn how to design and architect Spring Boot microservices that leverage Kubernetes and scale to millions of requests.`,
 }
 
 export function Register(store: Store<State, Actions>) {
@@ -19,18 +19,18 @@ export function Register(store: Store<State, Actions>) {
       id: 'og-02k8s-java',
       pageId: ZeroToK8sJava.id,
       image: (
-        <img src='src/02k8s-java/jury.jpg' alt='Hands-on guide: developing and deploying Java apps in Kubernetes' />
+        <img src='src/02k8s-java/jury.png' alt='Hands-on guide: developing and deploying Java apps in Kubernetes' />
       ),
       title: 'Hands-on guide: developing and deploying Java apps in Kubernetes',
-      description: `Learning how to design and architect applications that leverage Kubernetes is the most valuable skill that you could learn to be successful in deploying and scaling your traffic to millions of requests and beyond.`,
+      description: `Learning how to design and architect Spring Boot microservices that leverage Kubernetes is the most valuable skill that you could learn to be successful in deploying and scaling your traffic to millions of requests and beyond.`,
     }),
   )
   store.dispatch(
     Action.registerBlogPost({
       id: 'bp-02k8s-java',
       pageId: ZeroToK8sJava.id,
-      authorId: Authors.danielePolencic.id,
-      description: `Learning how to design and architect applications that leverage Kubernetes is the most valuable skill that you could learn to be successful in deploying and scaling your traffic to millions of requests and beyond.`,
+      authorId: Authors.mauricioSalatino.id,
+      description: `Learning how to design and architect Spring Boot microservicesleverage Kubernetes is the most valuable skill that you could learn to be successful in deploying and scaling your traffic to millions of requests and beyond.`,
       title: 'Hands-on guide: developing and deploying Java apps in Kubernetes',
       publishedDate: '2020-03-11',
 
@@ -47,7 +47,7 @@ export function Register(store: Store<State, Actions>) {
   )
   store.dispatch(
     Action.registerPreviewPicture({
-      id: 'v-picture',
+      id: '02k8s-java-picture',
       pageId: ZeroToK8sJava.id,
       image: <img src='src/02k8s-java/jury.svg' alt={ZeroToK8sJava.title} />,
     }),
