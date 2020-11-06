81e366e6f66c8b5b928d14cbb07960fbf32098c3
Amit Saha <amitsaha@users.noreply.github.com>
2020-11-06 17:23:35 +1100

    Update micros-authentication.tsx

--- src/microsAuthentication/micros-authentication.tsx
+++ src/microsAuthentication/micros-authentication.tsx
@@ -4,30 +4,30 @@ import { join } from 'path'
 import { toVFile } from '../files'
 
 export const microsAuthentication = {
-  id: 'yaml-validation',
-  url: '/validating-kubernetes-yaml',
-  title: 'Validating Kubernetes YAML for best practice and policies',
-  description: `How can you prevent deployments that don't follow best practices from reaching the cluster? In this article you will compare six tools to validate Kubernetes YAML files.`,
+  id: 'micros-authentication',
+  url: '/microservices-authentication-kubernetes',
+  title: 'Authenticating microservices requests in Kubernetes',
+  description: `Authenticating microservices requests in Kubernetes.`,
 }
 
 export function Register(store: Store) {
-  store.dispatch(Action.pages.add(YamlValidation))
+  store.dispatch(Action.pages.add(microsAuthentication))
   store.dispatch(
     Action.openGraphs.add({
-      id: 'og-yaml-validation',
-      pageId: YamlValidation.id,
+      id: 'og-micros-authentication',
+      pageId: microsAuthentication.id,
       imagePath: 'src/yaml-validation/yaml-validate.png',
-      title: YamlValidation.title,
-      description: YamlValidation.description,
+      title: MicrosAuthentication.title,
+      description: MicrosAuthentication.description,
     }),
   )
   store.dispatch(
     Action.blogPosts.add({
-      id: 'bp-yaml-validation',
-      pageId: YamlValidation.id,
+      id: 'bp-micros-authentication',
+      pageId: MicrosAuthentication.id,
       authorId: Authors.amitSaha.id,
-      description: YamlValidation.description,
-      title: YamlValidation.title,
+      description: MicrosAuthentication.description,
+      title: MicrosAuthentication.title,
       publishedDate: '2020-06-17',
 
       content: toVFile({ path: join(__dirname, 'content.md') }),
@@ -35,15 +35,15 @@ export function Register(store: Store) {
   )
   store.dispatch(
     Action.tags.add({
-      id: YamlValidation.id + '-general-post',
+      id: MicrosAuthentication.id + '-general-post',
       tag: 'general-post',
-      pageId: YamlValidation.id,
+      pageId: MicrosAuthentication.id,
     }),
   )
   store.dispatch(
     Action.previewPictures.add({
       id: 'yaml-validation-picture',
-      pageId: YamlValidation.id,
+      pageId: MicrosAuthentication.id,
       imagePath: 'src/yaml-validation/yaml-validate.svg',
     }),
   )
